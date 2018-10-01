80b0bb230c92850786ddd3648b85947dd5fe7d75
Dimitry Ivanov <2@ivanoff.org.ua>
2018-10-01 21:28:40 +0300

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# App
+
+## Installation
+
+`git clone https://github.com/user/app.git`
+
+`cd app`
+
+`npm install`
+
+
+## Testing (optional)
+
+```
+npm test
+```
+
+## Usage
+
+```
+node app.js [options]
+```
+
+## Example
+
+```
+node app.js
+```
+
+## License
+
+Licensed under [MIT License](LICENSE).
+
+
+## Created by
+
+App's author
---
+++ test/part.js
@@ -0,0 +1,25 @@
+'use strict';
+var chai = require('chai');
+var expect = chai.expect;
+
+const Part = require('../lib/part');
+let part;
+
+describe('Part', () => {
+
+  const word = 'Hey';
+
+  beforeEach(() => {
+    part = new Part({word});
+  })
+
+  afterEach(() => {
+    part = undefined;
+  })
+
+  it('Part returned true', async () => {
+    const result = part.say({repeat: 2});
+    expect(result).is.true;
+  })
+
+})
